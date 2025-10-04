ba1e1d85f07d89396feb963ec3b3c120e5f4d7f5
Jeronimo Quintero Gomez <jeronimoq18@gmail.com>
2025-10-04 12:02:17 -0500

    first commit

---
+++ src/main/java/com/familyspences/budgetprocess/BudgetprocessApplication.java
@@ -0,0 +1,13 @@
+package com.familyspences.budgetprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetprocessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/familyspences/budgetprocess/BudgetprocessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyspences.budgetprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetprocessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
